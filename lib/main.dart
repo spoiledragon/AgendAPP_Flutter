482efee22a142707cc2a1dac48b8dc9fc14f8eb5
spoiledragon <karlo.gallegos2031@alumnos.udg.mx>
2022-04-12 12:16:49 -0500

    Comenzamos la app decente

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:agendapp/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+      title: 'Email and Password Login',
+      theme: ThemeData(
+
+        primarySwatch: Colors.red,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
+
+
+
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,59 @@
+
+import "package:flutter/material.dart";
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  //form key
+  final _formKey = GlobalKey<FormState>();
+  //editing controller
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    //email field
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+      //validator: (){},
+      onSaved: (value) {
+        emailController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+    );
+    //Password  field
+
+    final passwordField = TextFormField(
+      autofocus: false,
+      controller: passwordController,
+      //validator: (){},
+      onSaved: (value) {
+        passwordController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [passwordField, emailField],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
