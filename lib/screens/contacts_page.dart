02223370c367ca54da9d90d5bf16c79cb3586c0b
spoiledragon <karlo.gallegos2031@alumnos.udg.mx>
2022-05-11 17:28:29 -0500

    Lista Terminada con los datos de usuarios falta agregar y editar

--- lib/screens/contacts_page.dart
+++ lib/screens/contacts_page.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: camel_case_types, use_key_in_widget_constructors, prefer_const_constructors_in_immutables, prefer_const_constructors, duplicate_ignore, prefer_const_literals_to_create_immutables
 
-import 'package:agendapp/clases/user_class.dart';
-import 'package:agendapp/widget_done/miniContacto.dart';
+import 'package:agendapp/clases/contact_class.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class contact_page extends StatefulWidget {
   final String id;
@@ -15,6 +16,11 @@ class _contact_pageState extends State<contact_page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: Icon(Icons.refresh),
+      ),
       appBar: AppBar(
         // ignore: prefer_const_constructors
         title: Center(child: Text("Contactos")),
@@ -28,36 +34,106 @@ class _contact_pageState extends State<contact_page> {
               ))
         ],
       ),
-      body: Contact_List(),
+      body: Contact_List(widget.id),
     );
   }
 }
 
 class Contact_List extends StatefulWidget {
+  final String id;
+  String id_todelete = "";
+  Contact_List(this.id);
   @override
   State<Contact_List> createState() => _Contact_ListState();
 }
 
 class _Contact_ListState extends State<Contact_List> {
-  bool loading = false;
-
-  List<User> users = [];
+  bool loading = true;
+  List<Contact> contacts = [];
   @override
   void initState() {
     loading = true;
+    _loadUser(widget.id);
     // TODO: implement initState
     super.initState();
   }
 
+  
+void _loadUser(id) async {
+    var url =
+        "https://thelmaxd.000webhostapp.com/Agendapp/contactos.php?userID=" +
+            id;
+    Response response = await get(Uri.parse(url));
+    List<Contact> _contacts = [];
+    var userResponse = json.decode(response.body);
+    for (var Contacto in userResponse) {
+      _contacts.add(Contact.fromJson(Contacto));
+    }
+    setState(() {
+      contacts = _contacts;
+      loading = false;
+    });
+  
+    //aqui sacamos los datos y los metemos a un array
+  }
+
+  void _delete_user(id) async {
+    var url =
+        "https://thelmaxd.000webhostapp.com/Agendapp/contact_todelete.php?ID=" +
+            contacts[id].id;
+    Response response = await get(Uri.parse(url));
+    List<Contact> _contacts = [];
+    //aqui sacamos los datos y los metemos a un array
+    var userResponse = json.decode(response.body);
+    print(userResponse);
+  }
+
+  void _showModalBottomSheet(BuildContext context, int id) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.edit),
+                title: Text('Edit name'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.delete),
+                title: Text('Delete'),
+                onTap: () {
+                  Navigator.pop(context);
+                  _delete_user(id);
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  //LO QUE SI SE VE
   @override
   Widget build(BuildContext context) {
+    if (loading) {
+      return Center(child: CircularProgressIndicator());
+    }
     return ListView.builder(
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text("Titulo $index"),
-            subtitle: Text("Hola"),
+          return GestureDetector(
+            onTap: () => {_showModalBottomSheet(context, index)},
+            child: ListTile(
+              title: Text(contacts[index].name),
+              subtitle: Text(contacts[index].email),
+              leading: CircleAvatar(
+                  backgroundImage: NetworkImage(contacts[index].photoUrl)),
+              trailing: Text(contacts[index].tel),
+            ),
           );
         },
-        itemCount: 100);
+        itemCount: contacts.length);
   }
 }
